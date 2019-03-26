eec3cc65a13fa1e44a64f94635be75faf78bd4d9
philipgreat <philip_chang@163.com>
2019-03-26 15:31:41 +0800

    upgrade to new UI

--- bizui/src/bizcomponents/hospital/Hospital.dashboard.js
+++ bizui/src/bizcomponents/hospital/Hospital.dashboard.js
@@ -5,7 +5,7 @@ import FontAwesome from 'react-fontawesome';
 import { connect } from 'dva'
 import moment from 'moment'
 import BooleanOption from 'components/BooleanOption';
-import { Row, Col, Icon, Card, Tabs, Table, Radio, DatePicker, Tooltip, Menu, Dropdown,Badge, Switch,Select,Form,AutoComplete,Modal } from 'antd'
+import { Statistic, Row, Col, Icon, Card, Tabs, Table, Radio, DatePicker, Tooltip, Menu, Dropdown,Badge, Switch,Select,Form,AutoComplete,Modal ,Button} from 'antd'
 import { Link, Route, Redirect} from 'dva/router'
 import numeral from 'numeral'
 import {
@@ -21,13 +21,13 @@ import ImagePreview from '../../components/ImagePreview';
 import GlobalComponents from '../../custcomponents';
 import DashboardTool from '../../common/Dashboard.tool'
 import appLocaleName from '../../common/Locale.tool'
-
+const ButtonGroup = Button.Group;
 const {aggregateDataset,calcKey, defaultHideCloseTrans,
   defaultImageListOf,defaultSettingListOf,defaultBuildTransferModal,
   defaultExecuteTrans,defaultHandleTransferSearch,defaultShowTransferModel,
   defaultRenderExtraHeader,
   defaultSubListsOf,
-  defaultRenderExtraFooter,renderForTimeLine,renderForNumbers
+  defaultRenderExtraFooter,renderForTimeLine,renderForNumbers,defaultQuickFunctions
 }= DashboardTool
 
 
@@ -78,18 +78,46 @@ const internalSummaryOf = (hospital,targetComponent) =>{
 	const {HospitalService} = GlobalComponents
 	const userContext = null
 	return (
-	<DescriptionList className={styles.headerList} size="small" col="4">
-<Description term="ID">{hospital.id}</Description> 
-<Description term="名称">{hospital.name}</Description> 
-<Description term="地址">{hospital.address}</Description> 
-<Description term="电话">{hospital.telephone}</Description> 
+  <DescriptionList className={styles.headerList} size="small" col="4">
+  <Description term="ID">{hospital.id}</Description> 
+  <Description term="名称">{hospital.name}</Description> 
+  <Description term="地址">{hospital.address}</Description> 
+  <Description term="电话">{hospital.telephone}</Description> 
 	
-        {buildTransferModal(hospital,targetComponent)}
-      </DescriptionList>
+  {buildTransferModal(hospital,targetComponent)}
+</DescriptionList>
 	)
 
 }
 
+/*
+
+<ButtonGroup>
+            <Button onClick={this.decline}>
+              <Icon type="minus" />
+            </Button>
+            <Button onClick={this.increase}>
+              <Icon type="plus" />
+            </Button>
+          </ButtonGroup>
+
+
+          title={
+                <span>
+                  <Link to={`/${cardsData.cardsFor}/${id}/list/${item.name}/${item.displayName}列表`}>
+                  {item.displayName}({numeral(item.count).format('0,0')})
+                
+                </Link>
+                  { (
+                  <Link to={`/${cardsData.cardsFor}/${id}/list/${item.role}CreateForm`}>
+                    <span className={styles.splitLine} />
+                    <FontAwesome name="plus" />
+                    {appLocaleName(userContext, 'Add')}
+                  </Link>)}
+                </span>}
+*/
+
+const internalQuickFunctions = defaultQuickFunctions
 
 class HospitalDashboard extends Component {
 
@@ -102,7 +130,7 @@ class HospitalDashboard extends Component {
     transferServiceName:"",
     currentValue:"",
     transferTargetParameterName:"",  
-    defaultType: 'hospital'
+    defaultType: 'hospital',
 
 
   }
@@ -136,21 +164,24 @@ class HospitalDashboard extends Component {
     const summaryOf = this.props.summaryOf || internalSummaryOf
     const renderTitle = this.props.renderTitle || internalRenderTitle
     const renderExtraFooter = this.props.renderExtraFooter || internalRenderExtraFooter
+    const quickFunctions = this.props.quickFunctions || internalQuickFunctions
     return (
-
+      
       <PageHeaderLayout
         title={renderTitle(cardsData,this)}
         content={summaryOf(cardsData.cardsSource,this)}
         wrapperClassName={styles.advancedForm}
       >
-      {renderExtraHeader(cardsData.cardsSource)}
-        <div>
+      
+       
+        {quickFunctions(cardsData)} 
+        {renderExtraHeader(cardsData.cardsSource)}
         {settingListOf(cardsData.cardsSource)}
         {imageListOf(cardsData.cardsSource)}
-        {subListsOf(cardsData)} 
+        
         {largeTextOf(cardsData.cardsSource)}
           
-        </div>
+        
       </PageHeaderLayout>
     )
   }

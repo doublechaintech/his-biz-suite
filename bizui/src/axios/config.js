ac904d66030af2bc1a22443a722383bae5370ee0
philipgreat <philip_chang@163.com>
2019-03-27 10:09:14 +0800

    Update to new UI

--- bizui/src/axios/config.js
+++ bizui/src/axios/config.js
@@ -1,7 +1,7 @@
 
 export const SYSTEM_SHORT_NAME = "his" //Use for requssting different apps
 export const SYSTEM_LOCAL_NAME = "医生排班系统" 
-export const BUILD_TIIME="Sat Mar 23 20:56:01 CST 2019"
+export const BUILD_TIIME="Tue Mar 26 15:42:00 CST 2019"
 export const MEDIA_UPLOAD_URL="https://www.doublechaintech.com/mss/upload.html"
 export const MEDIA_PREFIX="https://www.doublechaintech.com/mss/"
 

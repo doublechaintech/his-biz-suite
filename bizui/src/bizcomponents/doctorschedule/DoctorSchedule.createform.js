b14ae43485883cf6c9daf7dba0c622e78872fd3f
philipgreat <philip_chang@163.com>
2019-04-02 14:04:08 +0800

    Add icon

--- bizui/src/bizcomponents/doctorschedule/DoctorSchedule.createform.js
+++ bizui/src/bizcomponents/doctorschedule/DoctorSchedule.createform.js
@@ -18,9 +18,9 @@ const testValues = {};
 /*
 const testValues = {
   name: '2019年3月11日魏松全在内分泌科坐班收诊疗费,每个10',
-  scheduleDate: '2018-05-29',
-  available: '15',
-  price: '87.52',
+  scheduleDate: '2018-05-22',
+  available: '18',
+  price: '93.88',
   doctorId: 'D000001',
   periodId: 'P000001',
   departmentId: 'D000001',

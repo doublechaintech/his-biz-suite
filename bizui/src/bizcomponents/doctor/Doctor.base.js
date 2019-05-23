42e747214ba3d87bb694885665d3748a91fdf8d5
philipgreat <philip_chang@163.com>
2019-05-23 14:26:15 +0800

    fix the issue when there is no sublist

--- bizui/src/bizcomponents/doctor/Doctor.base.js
+++ bizui/src/bizcomponents/doctor/Doctor.base.js
@@ -47,15 +47,9 @@ const fieldLabels = {
 }
 
 const displayColumns = [
-<<<<<<< HEAD
   { title: fieldLabels.id, debugtype: 'string', dataIndex: 'id', width: '20', render: (text, record)=>renderTextCell(text,record,'doctor') , sorter: true },
   { title: fieldLabels.name, debugtype: 'string', dataIndex: 'name', width: '7',render: (text, record)=>renderTextCell(text,record)},
   { title: fieldLabels.shotImage, dataIndex: 'shotImage', render: (text, record) => renderImageCell(text,record,'拍摄的图像') },
-=======
-  { title: fieldLabels.ID, debugtype: 'string', dataIndex: 'id', width: '20', render: (text, record)=>renderTextCell(text,record,'doctor') , sorter: true },
-  { title: fieldLabels.name, debugtype: 'string', dataIndex: 'name', width: '7',render: (text, record)=>renderTextCell(text,record), sorter:true },
-  { title: fieldLabels.shotImage, dataIndex: 'shotImage', render: (text, record) => renderImageCell(text,record,'拍摄的图像'), sorter:true },
->>>>>>> 7c90622958711652c7eaa90ae743f4f09554e519
   { title: fieldLabels.hospital, dataIndex: 'hospital', render: (text, record) => renderReferenceCell(text, record), sorter:true},
   { title: fieldLabels.updateTime, dataIndex: 'updateTime', render: (text, record) =>renderDateTimeCell(text,record), sorter: true},
 
